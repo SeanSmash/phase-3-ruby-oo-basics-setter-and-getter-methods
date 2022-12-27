cb538477eaeafa4a842295b43995796ed164f6a6
Sean Sarokas <sps4462@gmail.com>
2022-12-27 00:05:09 -0500

    commit tree e4a0bc6d7fa753c8c98ba032722094577853a7fd

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
+    attr_accessor :job
+    
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
 
 end
